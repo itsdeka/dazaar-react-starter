389fc880cd873a8c990843b7bfa0915a6a74940f
Dario Moceri <dario.moceri@bitfinex.com>
2021-05-08 15:35:25 +0200

    bump

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+const hypercore = require("hypercore");
+const pump = require("pump");
+const market = require("dazaar");
+const racf = require("random-access-chrome-file");
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const m = market("tmp");
+
+    var feed = hypercore(function (filename) {
+      return racf(filename);
+    });
+
+    feed.append("valuable");
+
+    const seller = m.sell(feed, {
+      validate(remoteKey, cb) {
+        console.log("this key wants our hypercore", remoteKey);
+        cb(null);
+      },
+    });
+
+    seller.ready(function (err) {
+      if (err) throw err; // Do proper error handling
+      console.log("seller key pair fully loaded ...");
+
+      const buyer = m.buy(seller.key);
+
+      buyer.on("feed", function () {
+        console.log("got the feed!");
+        buyer.feed.get(0, function (err, data) {
+          if (err) throw err; // Do proper error handling
+          console.log("first feed entry: " + data);
+        });
+      });
+
+      buyer.on("validate", function () {
+        console.log("remote validated us");
+      });
+
+      const stream = seller.replicate();
+
+      pump(stream, buyer.replicate(), stream, function (err) {
+        console.log("replication ended", err);
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Check console logs to see more...
     </div>
   );
-}
+};
 
 export default App;
